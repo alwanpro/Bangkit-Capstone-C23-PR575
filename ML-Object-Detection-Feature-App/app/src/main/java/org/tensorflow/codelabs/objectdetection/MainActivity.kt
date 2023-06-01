6bf9a3b3ca81a12dc3fb74a593257e31f71a5cde
[Arcfoz] <[ikramtauffiqulh@gmail.com]>
2023-06-01 11:32:51 +0700

    Update app

--- ML-Object-Detection-Feature-App/app/src/main/java/org/tensorflow/codelabs/objectdetection/MainActivity.kt
+++ ML-Object-Detection-Feature-App/app/src/main/java/org/tensorflow/codelabs/objectdetection/MainActivity.kt
@@ -1,23 +1,8 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.tensorflow.codelabs.objectdetection
 
 import android.app.Activity
 import android.content.ActivityNotFoundException
+import android.content.ClipDescription
 import android.content.Intent
 import android.graphics.*
 import android.net.Uri
@@ -59,6 +44,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var imgSampleThree: ImageView
     private lateinit var tvPlaceholder: TextView
     private lateinit var currentPhotoPath: String
+    private lateinit var tvDescription : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,6 +56,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         imgSampleTwo = findViewById(R.id.imgSampleTwo)
         imgSampleThree = findViewById(R.id.imgSampleThree)
         tvPlaceholder = findViewById(R.id.tvPlaceholder)
+        tvDescription = findViewById(R.id.tvDescription)
 
         captureImageFab.setOnClickListener(this)
         imgSampleOne.setOnClickListener(this)
@@ -133,6 +120,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         // Step 3: Feed given image to the detector
         val results = detector.detect(image)
 
+        val textOutput = results.map { it.categories.first().label }
+
         // Step 4: Parse the detection result and show it
         val resultToDisplay = results.map {
             // Get the top-1 category and craft the display text
@@ -147,6 +136,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         runOnUiThread {
             inputImageView.setImageBitmap(imgWithResult)
         }
+        tvDescription.text = "High Prediction : ${textOutput[0]}"
         debugPrint(results)
     }
 
