9250fd5eb3f324307d64a497bd08c48d42066873
[Arcfoz] <[ikramtauffiqulh@gmail.com]>
2023-06-06 10:36:33 +0700

    update scan app

---
+++ ML-Object-Detection-Feature-App/app/src/main/java/com/nutriku/scan_nutriku_noncompose/SelectActivity.kt
@@ -0,0 +1,138 @@
+package com.nutriku.scan_nutriku_noncompose
+
+import android.Manifest
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.nutriku.scan_nutriku_noncompose.databinding.ActivitySelectBinding
+import com.nutriku.scan_nutriku_noncompose.scan.CameraActivity
+import com.nutriku.scan_nutriku_noncompose.scan.ScanResultActivity
+import com.nutriku.scan_nutriku_noncompose.utils.rotateBitmapBase
+import com.nutriku.scan_nutriku_noncompose.utils.uriToFile
+import java.io.File
+import java.io.FileOutputStream
+
+
+class SelectActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySelectBinding
+    private var getMyFile: File? = null
+
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+
+        const val TAG = "TFLite - ODT"
+        const val REQUEST_IMAGE_CAPTURE: Int = 1
+        private const val MAX_FONT_SIZE = 96F
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        binding.camera.setOnClickListener {
+            startCameraX()
+        }
+        binding.gallery.setOnClickListener{
+            startGallery()
+        }
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            getMyFile = myFile
+            val result = rotateBitmapBase(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+
+            // Save the Bitmap to a file
+            val file = File(cacheDir, "image.jpg")
+            val fileOutputStream = FileOutputStream(file)
+            result.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+            fileOutputStream.close()
+
+            val intent = Intent(this, ScanResultActivity::class.java)
+            intent.putExtra("imagePath", file.absolutePath)
+            startActivity(intent)
+
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@SelectActivity)
+            getMyFile = myFile
+
+            val bitmap = uriToBitmap(contentResolver, selectedImg)
+            if (bitmap != null) {
+                // Save the Bitmap to a file
+                val file = File(cacheDir, "image.jpg")
+                val fileOutputStream = FileOutputStream(file)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+                fileOutputStream.close()
+
+                val intent = Intent(this, ScanResultActivity::class.java)
+                intent.putExtra("imagePath2", file.absolutePath)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun uriToBitmap(contentResolver: ContentResolver, uri: Uri): Bitmap? {
+        return try {
+            // Open an input stream from the URI
+            val inputStream = contentResolver.openInputStream(uri)
+
+            // Decode the input stream into a Bitmap
+            BitmapFactory.decodeStream(inputStream)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
---
+++ ML-Object-Detection-Feature-App/app/src/main/java/com/nutriku/scan_nutriku_noncompose/scan/ScanResultActivity.kt
@@ -0,0 +1,172 @@
+package com.nutriku.scan_nutriku_noncompose.scan
+
+import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.nutriku.scan_nutriku_noncompose.databinding.ActivityScanresultBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.Detection
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
+
+class ScanResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScanresultBinding
+
+    companion object {
+
+        const val TAG = "TFLite - ODT"
+        const val REQUEST_IMAGE_CAPTURE: Int = 1
+        private const val MAX_FONT_SIZE = 96F
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityScanresultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imagePath = intent.getStringExtra("imagePath")
+        if (imagePath != null) {
+            val bitmap = BitmapFactory.decodeFile(imagePath)
+            // Use the bitmap as needed
+            binding.ivpicture.setImageBitmap(bitmap)
+            setViewAndDetect(bitmap)
+        }
+
+        val imagePath2 = intent.getStringExtra("imagePath2")
+        if (imagePath2 != null) {
+            val bitmap = BitmapFactory.decodeFile(imagePath2)
+            // Use the bitmap as needed
+            binding.ivpicture.setImageBitmap(bitmap)
+            setViewAndDetect(bitmap)
+        }
+
+    }
+
+
+
+    private fun setViewAndDetect(bitmap: Bitmap) {
+        // Display capture image
+        binding.ivpicture.setImageBitmap(bitmap)
+        binding.tvPlaceholder.visibility = View.INVISIBLE
+
+        // Run ODT and display result
+        // Note that we run this in the background thread to avoid blocking the app UI because
+        // TFLite object detection is a synchronised process.
+        lifecycleScope.launch(Dispatchers.Default) { runObjectDetection(bitmap) }
+    }
+
+    private fun runObjectDetection(bitmap: Bitmap) {
+        // Step 1: Create TFLite's TensorImage object
+        val image = TensorImage.fromBitmap(bitmap)
+
+        // Step 2: Initialize the detector object
+        val options = ObjectDetector.ObjectDetectorOptions.builder()
+            .setMaxResults(5)
+            .setScoreThreshold(0.3f)
+            .build()
+        val detector = ObjectDetector.createFromFileAndOptions(
+            this,
+            "model.tflite",
+            options
+        )
+
+        // Step 3: Feed given image to the detector
+        val results = detector.detect(image)
+
+        val textOutput = results.map { it.categories.first().label }
+
+        // Step 4: Parse the detection result and show it
+        val resultToDisplay = results.map {
+            // Get the top-1 category and craft the display text
+            val category = it.categories.first()
+            val text = "${category.label}, ${category.score.times(100).toInt()}%"
+
+            // Create a data object to display the detection result
+            DetectionResult(it.boundingBox, text)
+        }
+        // Draw the detection result on the bitmap and show it.
+        val imgWithResult = drawDetectionResult(bitmap, resultToDisplay)
+        runOnUiThread {
+            binding.ivpicture.setImageBitmap(imgWithResult)
+        }
+//        binding.tvOutput.text = "High Prediction : ${textOutput[0]}"
+//        binding.tvOutput.text = "${textOutput}"
+        if (textOutput.isEmpty()){
+            binding.tvDescription.text = "Tidak dapat mengidentifikasi makanan"
+        } else {
+            binding.tvDescription.text = "${textOutput[0]}"
+        }
+        debugPrint(results)
+    }
+
+    data class DetectionResult(val boundingBox: RectF, val text: String)
+
+    private fun drawDetectionResult(
+        bitmap: Bitmap,
+        detectionResults: List<DetectionResult>
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        detectionResults.forEach {
+            // draw bounding box
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = it.boundingBox
+            canvas.drawRect(box, pen)
+
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            // calculate the right font size
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = ScanResultActivity.MAX_FONT_SIZE
+            pen.getTextBounds(it.text, 0, it.text.length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            // adjust the font size so texts are inside the bounding box
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+            canvas.drawText(
+                it.text, box.left + margin,
+                box.top + tagSize.height().times(1F), pen
+            )
+        }
+        return outputBitmap
+    }
+
+    private fun debugPrint(results : List<Detection>) {
+        for ((i, obj) in results.withIndex()) {
+            val box = obj.boundingBox
+
+            Log.d(TAG, "Detected object: ${i} ")
+            Log.d(TAG, "  boundingBox: (${box.left}, ${box.top}) - (${box.right},${box.bottom})")
+
+            for ((j, category) in obj.categories.withIndex()) {
+                Log.d(ScanResultActivity.TAG, "    Label $j: ${category.label}")
+                val confidence: Int = category.score.times(100).toInt()
+                Log.d(TAG, "    Confidence: ${confidence}%")
+            }
+        }
+    }
+}
